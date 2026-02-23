5e03d243e173fdf7d177f0f72c91a585a2410167
Anshuman Jadiya <anshuman.jadiya03@gmail.com>
2026-02-23 15:13:24 +0530

    Add uptime endpoint to check server status

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Simple endpoint for UptimeRobot ping
 app.get('/', (req, res) => {
   res.send('Javion is running!');
